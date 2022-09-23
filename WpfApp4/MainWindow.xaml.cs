e9689056157ad062baec1d6ca4313d78b5b5b742
Trapland <89914789+Trapland@users.noreply.github.com>
2022-09-23 11:20:08 +0200

    Добавьте файлы проекта.

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void b_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                media.Source = new Uri(ofd.FileName);
+                s1.Maximum = 109;
+            }
+        }
+
+        private void b2_Click(object sender, RoutedEventArgs e)
+        {
+            media.LoadedBehavior = MediaState.Play;
+            if (media.NaturalDuration.HasTimeSpan)
+            {
+                var a = media.NaturalDuration.TimeSpan.TotalSeconds;
+                s1.Maximum = Convert.ToDouble(a);
+            }
+        }
+
+        private void b3_Click(object sender, RoutedEventArgs e)
+        {
+            media.LoadedBehavior = MediaState.Pause;
+        }
+
+        private void b4_Click(object sender, RoutedEventArgs e)
+        {
+            media.LoadedBehavior = MediaState.Stop;
+        }
+
+        private void s1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+            media.Position = TimeSpan.FromSeconds(s1.Value);
+        }
+
+        private void s1_ManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
+        {
+            
+        }
+    }
+}
